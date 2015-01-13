32e8a2c38150c219e7d156e0b9b3292ec61c966f
Karsten Knese <kknese@aldebaran.com>
2015-01-13 16:56:56 +0100

    adding simple publisher

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <ros/ros.h>
+#include <std_msgs/Int32.h>
 
 #include "alrosconverter.hpp"
 
@@ -14,16 +15,39 @@ int main(int argc, char *argv[])
   app.start();
   qi::SessionPtr session = app.session();
 
-  // tmp master ip
+  // temporary debug master ip
   const std::string& ip = "http://10.0.132.69:11311";
   qi::Object<ALRosConverter> converter( new ALRosConverter(ip) );
 
+  session->registerService("ALRosConverter", converter);
+  //app.run();
+
+
+  /*
+  * ROS Stuff
+  */
   ros::init( argc, argv, "alrosconverter" );
   std::cout << "alrosconverter init" << std::endl;
 
-  ros::NodeHandle nh;
+  /*
+  * registers the node at ROS Master's side
+  * verify with: rosnode list
+  */
+  ros::NodeHandle nh("~");
   std::cout << "node handle setup" << std::endl;
 
-  session->registerService("ALRosConverter", converter);
-  app.run();
+  ros::Publisher pub = nh.advertise< std_msgs::Int32 >( "chatter", 100 );
+  std_msgs::Int32 m;
+  m.data = 0;
+
+  ros::Rate r(1);
+  while( nh.ok() )
+  {
+    std::cout << "publishing " << m.data << std::endl;
+    pub.publish( m );
+    m.data++;
+
+    r.sleep();
+  }
+  std::cout << "shutting down .. " << std::endl;
 }
