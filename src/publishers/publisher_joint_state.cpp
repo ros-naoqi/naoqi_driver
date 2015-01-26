3676c515eb0bac18b47658e9f30cf63a5779a7c8
Karsten Knese <kknese@aldebaran.com>
2015-01-26 13:19:02 +0100

    add robot state publisher in code

--- src/publishers/publisher_joint_state.cpp
+++ src/publishers/publisher_joint_state.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/tree.hpp>
+
+#include <robot_state_publisher/robot_state_publisher.h>
+
 #include "publisher_joint_state.hpp"
 
 namespace alros
@@ -26,17 +31,48 @@ void JointStatePublisher::publish()
   msg_.header.stamp = ros::Time::now();
   std::cout << name() << " is publishing " << std::endl;
   pub_.publish( msg_ );
+
+  std::map< std::string, double > joint_state_map;
+  // stupid version --> change this with std::transform c++11 ??!
+  std::transform( msg_.name.begin(), msg_.name.end(), msg_.position.begin(), std::inserter( joint_state_map, joint_state_map.end() ), std::make_pair<std::string, double>);
+
+  static const std::string& jt_tf_prefix = "";
+  rspPtr_->publishTransforms( joint_state_map, msg_.header.stamp, jt_tf_prefix );
+  rspPtr_->publishFixedTransforms( jt_tf_prefix );
+
 }
 
 void JointStatePublisher::reset( ros::NodeHandle& nh )
 {
   pub_ = nh.advertise<sensor_msgs::JointState>( topic_, 10 );
 
+  // load urdf from param server (alternatively from file)
+  if ( nh.hasParam("/robot_description") )
+  {
+    KDL::Tree tree;
+    std::string robot_desc;
+    nh.getParam("/robot_description", robot_desc);
+    kdl_parser::treeFromString( robot_desc, tree );
+    rspPtr_.reset( new robot_state_publisher::RobotStatePublisher(tree) );
+  }
+  else{
+    std::cerr << "failed to load robot description in joint_state_publisher" << std::endl;
+    is_initialized_ = false;
+    return;
+  }
+
   msg_.name = p_motion_.call<std::vector<std::string> >("getBodyNames", "JointActuators" );
 
   is_initialized_ = true;
   std::cout << name() << " is resetting" << std::endl;
 }
+
+bool JointStatePublisher::isSubscribed() const
+{
+  // assume JS and TF as essential, so publish always
+  return true;
+}
+
 
 } //publisher
 } // alros
--- src/publishers/publisher_joint_state.hpp
+++ src/publishers/publisher_joint_state.hpp
@@ -1,13 +1,12 @@
 #ifndef JOINT_STATES_PUBLISHER_HPP
 #define JOINT_STATES_PUBLISHER_HPP
 
-
 /**
 * ROS includes
 */
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
-
+#include <robot_state_publisher/robot_state_publisher.h>
 /**
 * ALDEBARAN includes
 */
@@ -22,6 +21,7 @@ namespace alros
 namespace publisher
 {
 
+
 class JointStatePublisher : public BasePublisher
 {
 
@@ -33,9 +33,12 @@ public:
 
   void reset( ros::NodeHandle& nh );
 
+  bool isSubscribed() const;
+
 private:
   qi::AnyObject p_motion_;
   sensor_msgs::JointState msg_;
+  boost::shared_ptr<robot_state_publisher::RobotStatePublisher> rspPtr_;
 }; // class
 
 } //publisher
