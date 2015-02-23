b50d0399f1bad1d7cb8cbf1ee1b59462f7b5c320
Vincent Rabaud <vrabaud@aldebaran-robotics.com>
2015-02-23 13:52:32 +0100

    fix runtime problem

--- src/subscribers/moveto.cpp
+++ src/subscribers/moveto.cpp
@@ -20,6 +20,7 @@
 #include <math.h>
 
 #include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 
