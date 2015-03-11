f0762ba0bc9ea74d4335c5d7f3220f8582b06666
Karsten Knese <kknese@aldebaran.com>
2015-03-11 18:27:28 +0100

    first test for callback refactor

--- test/dynamic_callback.cpp
+++ test/dynamic_callback.cpp
@@ -5,49 +5,44 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <boost/function.hpp>
 #include <boost/bind.hpp>
-
-/**
- * Emulate different message types here
+#include <boost/foreach.hpp>
+#define for_each BOOST_FOREACH
+/*
+ * ROS
  */
-struct StringMsg
-{
-  std::string data;
-};
-struct FloatMsg
-{
-  float data;
-};
-struct IntMsg
-{
-  int data;
-};
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
 
 /**
  * Emulate different publishers here
  */
 struct StringPublisher
 {
-  void publish( const StringMsg& m )
+  void publish( const std_msgs::String& m )
   {
     std::cout << "String Publisher publishes " << m.data << std::endl;
   }
 };
-struct FloatPublisher
+struct Float32Publisher
 {
-  void publish( const FloatMsg& m )
+  void publish( const std_msgs::Float32& m )
   {
-    std::cout << "Float Publisher publishes " << m.data << std::endl;
+    std::cout << "Float32 Publisher publishes " << m.data << std::endl;
   }
 };
-struct IntPublisher
+struct Int32Publisher
 {
-  void publish( const IntMsg& m )
+  void publish( const std_msgs::Int32& m1, const std_msgs::Int32& m2 )
   {
-    std::cout << "Int Publisher publishes " << m.data << std::endl;
+    std::cout << "Int Publisher publishes two messages " << m1.data << " and " << m2.data << std::endl;
   }
 };
+
 /**
  * Emulate the recorder
  */
@@ -59,133 +54,152 @@ struct Recorder
     std::cout << "Recorder records " << m.data << std::endl;
   }
 
-  void record( const IntMsg& m )
+  template <typename T>
+  void record( const std_msgs::Int32& m1, const std_msgs::Int32& m2 )
   {
-    std::cout << "Recorder writes directly a cool InterMsg " << std::endl;
+    std::cout << "Recorder writes directly two cool InterMsg " << m1.data << " and " << m2.data << std::endl;
   }
+};
+
+
+enum MessageAction
+{
+  PUBLISH,
+  RECORD,
+  LOG
 };
 
 /**
  * Emulate the translation unit
  */
-
-struct StringConverter //: public BaseConverter
+struct StringConverter
 {
-  typedef boost::function<void(StringMsg)> Callback;
+  typedef boost::function<void(std_msgs::String)> Callback;
 
-  Callback callback_function_publish;
-  Callback callback_function_record;
+  std::map<MessageAction, Callback> callbacks;
 
-  StringConverter(Callback publish_callback, Callback record_callback)
-    : callback_function_publish(publish_callback)
-    , callback_function_record(record_callback)
+  StringConverter()
   {}
 
-  // should get a callback to the recorder and the 
-  void operator()( bool publish_enabled, bool record_enabled ) const
+  void registerCallback( MessageAction action, Callback cb )
   {
-    StringMsg m;
+    callbacks[action] = cb;
+  }
+
+  void operator()( const std::vector<MessageAction>& actions )
+  {
+    std_msgs::String m;
     m.data = "Hello String";
 
-    if ( publish_enabled )
-	    callback_function_publish(m);
-    if ( record_enabled )
-	    callback_function_record(m);
+    for_each( const MessageAction& action, actions )
+    {
+      callbacks[action](m);
+    }
   }
 };
 
-struct FloatConverter
+struct Float32Converter
 {
-  typedef boost::function<void(FloatMsg)> Callback;
+  typedef boost::function<void(std_msgs::Float32)> Callback;
 
-  Callback callback_function_publish;
-  Callback callback_function_record;
+  std::map<MessageAction, Callback> callbacks;
 
-  FloatConverter(Callback publish_callback, Callback record_callback)
-    : callback_function_publish(publish_callback)
-    , callback_function_record(record_callback)
+  Float32Converter()
   {}
 
-
-  void operator()( bool publish_enabled, bool record_enabled ) const
+  void registerCallback( MessageAction action, Callback cb )
   {
-    FloatMsg float_msg;
-    float_msg.data = 1.5f;
-    if ( publish_enabled )
-	    callback_function_publish(float_msg);
-    if ( record_enabled )
-	    callback_function_record(float_msg);
+    callbacks[action] = cb;
+  }
 
+  void operator()( const std::vector<MessageAction>& actions )
+  {
+    std_msgs::Float32 m;
+    m.data = 3.14561;
+
+    for_each( const MessageAction& action, actions )
+    {
+      callbacks[action](m);
+    }
   }
 };
 
-template<class MessageType>
-struct CallbackCluster
+/*
+ * make the map templated by the callback functions
+ * typedef booost::function<void(std_msgs::Int32, std_msgs::Int32)> Callback
+ * CallbackMap<Callback> callbacks
+ */
+
+struct Int32Converter
 {
-  typedef boost::function<void(MessageType)> Callback;
+  typedef boost::function<void(std_msgs::Int32, std_msgs::Int32)> Callback;
 
-  Callback callback_function_publish;
-  Callback callback_function_record;
+  std::map<MessageAction, Callback> callbacks;
 
- CallbackCluster(Callback publish_callback, Callback record_callback)
-    : callback_function_publish(publish_callback)
-    , callback_function_record(record_callback)
+  Int32Converter()
   {}
 
-
-  void operator()( const MessageType& msg, bool publish_enabled, bool record_enabled ) const
+  void registerCallback( MessageAction action, Callback cb )
   {
-    if ( publish_enabled )
-	    callback_function_publish(msg);
-    if ( record_enabled )
-	    callback_function_record(msg);
+    callbacks[action] = cb;
   }
 
-};
-
-struct IntConverter
-{
-  CallbackCluster<IntMsg> callbacks;
-
-  IntConverter(CallbackCluster::Callback publish_callback, CallbackCluster::Callback record_callback)
-    : callbacks(publish_callback, record_callback)
-  {}
-
-  void operator()( bool publish_enabled, bool record_enabled ) const
+  void operator()( const std::vector<MessageAction>& actions )
   {
-    IntMsg int_msg;
-    int_msg.data = 1;
-    callbacks(msg, publish_enabled, record_enabled );
-  }
+    std_msgs::Int32 m1;
+    m1.data = 123;
 
+    std_msgs::Int32 m2;
+    m2.data = 456;
+
+    for_each ( MessageAction action, actions )
+    {
+      callbacks[action]( m1, m2 );
+    }
+  }
 };
 
-void lol(int i, std::string k);
 
 int main()
 {
   StringPublisher sp;
-  FloatPublisher fp;
-  IntPublisher ip;
+  Float32Publisher fp;
+  Int32Publisher ip;
 
   Recorder rec;
 
-  std::vector< boost::function<void( bool, bool )> > converters;
-  converters.push_back( StringConverter( boost::bind(&StringPublisher::publish, &sp, _1), boost::bind(&Recorder::record<StringMsg>, &rec, _1) ) );
-  converters.push_back( FloatConverter( boost::bind(&FloatPublisher::publish, &fp, _1), boost::bind(&Recorder::record<FloatMsg>, &rec, _1) ) );
-  converters.push_back( IntConverter( boost::bind(&IntPublisher::publish, &ip, _1), boost::bind(&Recorder::record<IntMsg>, &rec, _1) ) );
+  StringConverter sc;
+  sc.registerCallback( PUBLISH, boost::bind(&StringPublisher::publish, &sp, _1) );
+  sc.registerCallback( RECORD, boost::bind(&Recorder::record<std_msgs::String>, &rec, _1) );
+
+  Float32Converter fc;
+  fc.registerCallback( PUBLISH, boost::bind(&Float32Publisher::publish, &fp, _1) );
+  fc.registerCallback( RECORD, boost::bind(&Recorder::record<std_msgs::Float32>, &rec, _1) );
+
+  Int32Converter ic;
+  ic.registerCallback( PUBLISH, boost::bind(&Int32Publisher::publish, &ip, _1, _2) );
+  boost::bind(&Recorder::record<std_msgs::Int32>, &rec, _1, _2);
+  //ic.registerCallback( RECORD, boost::bind(&Recorder::record<std_msgs::Int32>, &rec, _1, _2) );
+
+  std::vector< boost::function<void( std::vector<MessageAction> )> > converters;
+  converters.push_back( sc );
+  converters.push_back( fc );
+  converters.push_back( ic );
 
   bool publish_enabled = true;
   bool record_enabled = false;
 
+  std::vector< MessageAction > actions;
+  if (publish_enabled)
+  actions.push_back( PUBLISH );
+  if ( record_enabled)
+  actions.push_back( RECORD );
+
+
   for( size_t i=0; i< converters.size(); ++i)
   {
-	converters[i]( publish_enabled, record_enabled );
+    converters[i]( actions );
   }
-  //Recorder rec;
-  //rec.record(float_msg);
-  //rec.record(string_msg);
-  //rec.record(int_msg);
 
   return 0;
 }
