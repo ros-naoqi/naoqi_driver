8cfa1b1f76b8dd714d77e1d245581f385e2c3d6a
Marine CHAMOUX <mchamoux@aldebaran.com>
2015-04-13 10:03:52 +0200

    add converters for int/float/string memory key

---
+++ src/converters/memory/float.hpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015 Aldebaran
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef MEMORY_FLOAT_CONVERTER_HPP
+#define MEMORY_FLOAT_CONVERTER_HPP
+
+#include <map>
+#include <vector>
+
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+
+#include <boost/function.hpp>
+
+#include <qi/anyobject.hpp>
+
+#include <alrosbridge/message_actions.h>
+#include "../converter_base.hpp"
+
+namespace alros
+{
+namespace converter
+{
+
+class MemoryFloatConverter : public BaseConverter<MemoryFloatConverter>
+{
+  typedef boost::function<void(std_msgs::Float32&)> Callback_t;
+
+public:
+
+  MemoryFloatConverter( const std::string& name, const float& frequency, const qi::SessionPtr& session, const std::string& memory_key );
+
+  void reset();
+
+  void registerCallback( const message_actions::MessageAction action, Callback_t cb );
+
+  void callAll( const std::vector<message_actions::MessageAction>& actions );
+
+private:
+  void convert();
+
+private:
+  /** Memory key to retrieve data */
+  std::string memory_key_;
+  /** Memory (Proxy) configurations */
+  qi::AnyObject p_memory_;
+
+  std::map<message_actions::MessageAction, Callback_t> callbacks_;
+  std_msgs::Float32 msg_;
+
+}; // class
+
+} //publisher
+} // alros
+
+#endif
---
+++ src/publishers/float.hpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 Aldebaran
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef FLOAT_PUBLISHER_HPP
+#define FLOAT_PUBLISHER_HPP
+
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+
+#include "publisher_base.hpp"
+#include <qi/anyobject.hpp>
+
+namespace alros
+{
+namespace publisher
+{
+
+class FloatPublisher : public BasePublisher<FloatPublisher>
+{
+
+public:
+  FloatPublisher( const std::string& topic );
+
+  void publish( const std_msgs::Float32& msg );
+
+  void reset( ros::NodeHandle& nh );
+
+}; // class
+
+} //publisher
+} // alros
+
+#endif
