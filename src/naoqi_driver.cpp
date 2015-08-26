98ac3b30723e92b18c3b7b4d761ae1b3c2d81ba4
Karsten Knese <kknese@aldebaran.com>
2015-08-26 19:00:08 +0200

    fix: advertise service in global ns

--- src/naoqi_driver.cpp
+++ src/naoqi_driver.cpp
@@ -797,7 +797,7 @@ void Driver::registerService( service::Service srv )
 
 void Driver::registerDefaultServices()
 {
-  registerService( boost::make_shared<service::RobotConfigService>("robot config service", "get_robot_config", sessionPtr_) );
+  registerService( boost::make_shared<service::RobotConfigService>("robot config service", "/naoqi_driver/get_robot_config", sessionPtr_) );
 }
 
 std::vector<std::string> Driver::getAvailableConverters()
