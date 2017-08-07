a17dfae8fcf573c6ee5eb586bdff959b441d001e
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-08-07 21:21:32 +0900

    add services for get and set volume

--- src/helpers/driver_helpers.hpp
+++ src/helpers/driver_helpers.hpp
@@ -23,6 +23,12 @@
 
 #include <naoqi_bridge_msgs/RobotInfo.h>
 
+#include <naoqi_bridge_msgs/SetVolume.h>
+
+#include <naoqi_bridge_msgs/GetVolume.h>
+
+#include <std_srvs/Empty.h>
+
 #include <qi/applicationsession.hpp>
 
 namespace naoqi
@@ -35,6 +41,10 @@ namespace driver
 const robot::Robot& getRobot( const qi::SessionPtr& session );
 
 const naoqi_bridge_msgs::RobotInfo& getRobotInfo( const qi::SessionPtr& session );
+
+const bool& setVolume( const qi::SessionPtr& session, naoqi_bridge_msgs::SetVolumeRequest req );
+
+const int& getVolume( const qi::SessionPtr& session, naoqi_bridge_msgs::GetVolumeRequest req );
 
 } // driver
 } // helpers
---
+++ src/services/set_volume.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Aldebaran
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "set_volume.hpp"
+#include "../helpers/driver_helpers.hpp"
+
+namespace naoqi
+{
+namespace service
+{
+
+  SetVolumeService::SetVolumeService( const std::string& name, const std::string& topic, const qi::SessionPtr& session )
+    : name_(name),
+      topic_(topic),
+      session_(session)
+  {}
+
+  void SetVolumeService::reset( ros::NodeHandle& nh )
+  {
+    service_ = nh.advertiseService(topic_, &SetVolumeService::callback, this);
+  }
+
+  bool SetVolumeService::callback( naoqi_bridge_msgs::SetVolumeRequest& req, naoqi_bridge_msgs::SetVolumeResponse& resp )
+  {
+    resp.success = helpers::driver::setVolume(session_, req);
+    return true;
+  }
+
+
+}
+}
